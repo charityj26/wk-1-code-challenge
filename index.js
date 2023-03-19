71e68f22d7aee60d4bea1c6ae58adea071b53747
charity <charity.joseph@students.moringaschool.com>
2023-03-19 19:57:35 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+//write that prompts the user to input student marks.The input should be between 0 and 100.Then output the correct grade.
+//A>79,B-60 to 79,c-59 to 49,D-40 to 49,E- less 40.
+function generateGrade(marks){
+    //grading
+    let a = 40
+    function assignGrade(a){
+
+        if (a>=79){
+            console.log('scored A with marks ${a}')
+          }  else if(a<=60){
+
+            console.log('scored B with marks ${a}')
+          }else if (a>=49){
+            console.log('scored c with marks ${a}')
+          
+          }else if (a>=49){
+            console.log('scored D with marks ${a}')
+          }
+        }
+        assignGrade(a)
+
+    }
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+        
+           
+          
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
