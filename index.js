8487282a11e682641e3001b87efd7ed51b9632f2
charity <charity.joseph@students.moringaschool.com>
2023-03-28 12:14:01 +0300

    index.js

--- index.js
+++ index.js
@@ -30,8 +30,24 @@ function checkLimit(speed){
 
 checkLimit(900)
 
-           
-           
+//net salary
+
+function payeCalculator(){
+  let salary=parseFloat(prompt("Enter your gross salary"));
+  if(salary<=24000){
+    console.log(salary-salary*0.01);
+  }else if(salary>=24001 && salary===32333){
+    console.log(salary-salary*0.25);
+  }else{
+    salary>32333;
+    console.log(salary-salary*0.3);
+  }
+  let deductions=parseFloat(prompt("Enter your monthly deductions"));
+  const netPay=salary-deductions;
+  return prompt(`Your monthly salary amounts to ${netpay}`);
+
+}
+payeCalculator();
            
            
            
